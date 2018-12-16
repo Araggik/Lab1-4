9537daba6c4486f66020f9016220d4a5b0f0f5f4
Araggik <45770047+Araggik@users.noreply.github.com>
2018-12-16 10:47:51 +0500

    Add files via upload

---
+++ lab1.ps1
@@ -0,0 +1,32 @@
+﻿param ([string]$way="c:\test" ,[int]$num=32 )
+Set-Location $way
+[string]$test = $way+"\Less"
+If(!(Test-Path $test) ) 
+{New-Item -Path $way -Name "Less" -ItemType Directory}
+Get-ChildItem | Foreach-Object {
+ function Func ([string]$name=$_.BaseName)
+ {
+   [bool]$x = $true #
+   [int]$n=$name.Length
+   For([int]$i=0;($i -lt $n) -and $x;$i++)
+   {
+     if(($name[$i] -ge '0') -and ($name[$i] -le '9'))
+     { 
+      [string]$str=$name[$i]
+      $i++
+      while(($name[$i] -ge '0') -and ($name[$i] -le '9') -and ($i -lt $n) )
+      {
+        $str=$str+$name[$i]
+        $i++
+      }
+      [int]$t=[int]::Parse($str)
+      if($t -lt $num)
+      {
+      $x=$false #
+      } 
+     }
+   }
+   return !($x)
+ }
+ If( Func($_.BaseName) ) {Copy-Item ($_.FullName) -Destination $test}
+}
